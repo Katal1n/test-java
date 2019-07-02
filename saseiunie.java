c21f81f8a5edb4d6aafa874f2f9693ad058425bd
Katal1n <52464851+Katal1n@users.noreply.github.com>
2019-07-02 20:20:08 +0300

    Add files via upload

---
+++ saseiunie.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class saseiunie {
+
+	public static void main(String[] args) {
+
+		int n;
+		
+		Scanner in = new Scanner( System.in );
+		System.out.print("Enter an integer for n: ");
+		n = in.nextInt();
+		
+		
+		System.out.print( n + "^2 = " );
+		System.out.print( n * n );
+	}
+
+}
